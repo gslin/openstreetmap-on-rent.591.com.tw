04af3927615d9b67f98e87a108cf69a58f4675c9
Gea-Suan Lin <gslin@gslin.org>
2021-06-17 22:18:38 +0800

    Find the accurate address.

--- openstreetmap-on-rent.591.com.tw.user.js
+++ openstreetmap-on-rent.591.com.tw.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/openstreetmap-on-rent.591.com.tw
 // @match       https://rent.591.com.tw/home/*
 // @grant       GM_xmlhttpRequest
-// @version     0.20210617.0
+// @version     0.20210617.1
 // @author      Gea-Suan Lin <gslin@gslin.com>
 // @description Embed OpenStreetMap on rent.591.com.tw.
 // @require     https://unpkg.com/leaflet@1.7.1/dist/leaflet.js
@@ -21,7 +21,7 @@
                     return;
                 }
 
-                let addr_el = document.querySelector('div.address');
+                let addr_el = document.querySelector('div.address span + span');
                 if (!addr_el) {
                     return;
                 }
@@ -48,11 +48,10 @@
                         // leaflet map element
                         let map_el = document.createElement('div');
                         map_el.setAttribute('style', 'height: 400px; width: 800px;');
-                        addr_el.insertAdjacentElement('afterend', map_el);
+                        addr_el.parentElement.insertAdjacentElement('afterend', map_el);
 
                         let map = L.map(map_el).setView([r0.lat, r0.lon], 17);
                         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-                        // L.marker([r0.lat, r0.lon]).addTo(map).bindPopup('House').openPopup();
                     },
                     url: url,
                 });
